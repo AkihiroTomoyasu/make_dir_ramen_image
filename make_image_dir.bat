6c5000527af1931e65e4c0729ea34584667ecc47
AkihiroTomoyasu <58322450+AkihiroTomoyasu@users.noreply.github.com>
2020-03-06 11:46:09 +0900

    ファイル作成

--- make_image_dir.bat
+++ make_image_dir.bat
@@ -1,1 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
 
+for %%i in (m???????.jpg m???????.png m???????.gif) do (
+	: ファイル名格納
+	set f=%%i
+	: 格納するディレクトリパス作成
+	set d=!f:~0,3!\!f:~3,3!\!f:~6,3!\ramen
+	: ディレクトリ作成
+	mkdir !d!
+	: ファイル移動
+	move %%i !d!
+)
